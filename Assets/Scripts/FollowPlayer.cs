eb31c8beb6f7b70ff48a3c4bfb028394eb8836c5
frederik desmet <print.fdesmet@gmail.com>
2021-01-12 16:02:54 +0100

    finished prototype 1 & challenge

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Create reference to an object in unity. => Vehicle
+    public GameObject player;
+    // Create a vector to hold the camera position.
+    private Vector3 offset = new Vector3(0, 5, -7);
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Sets the position to the camera equal to the referenced object and applies the offset.
+        transform.position = player.transform.position + offset;
+       
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float speed = 10.0f;
+    private float turnSpeed = 20;
+    private float _horizontalInput;
+    private float _forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the user input for horizontal movement.
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _forwardInput = Input.GetAxis("Vertical");
+        
+        // Move the vehicle based on Vertical input.
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed * _forwardInput));
+        
+        // Rotates the Vehicle based on horizontal input.
+        transform.Rotate(Vector3.up, turnSpeed * _horizontalInput * Time.deltaTime);
+    }
+}
