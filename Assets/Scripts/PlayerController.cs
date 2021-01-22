ca213ea1619ba528f600e2ced9831ecd7faccb23
frederik desmet <info@neo-level.com>
2021-01-22 13:33:28 +0100

    Added a speedometer.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     // Allows to interact with the variable in the unity editor while staying private.
+    [SerializeField] private float _speed;
     [SerializeField] private float _horsePower = 0.0f;
+    
     [SerializeField] private GameObject centerOfMass;
+    [SerializeField] private TextMeshProUGUI speedometerText;
     
     
     private float turnSpeed = 20;
@@ -20,11 +24,10 @@ public class PlayerController : MonoBehaviour
     {
         _carRigidbody = GetComponent<Rigidbody>();
         _carRigidbody.centerOfMass = centerOfMass.transform.position;
-
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         // Get the user input for horizontal movement.
         _horizontalInput = Input.GetAxis("Horizontal");
@@ -34,5 +37,9 @@ public class PlayerController : MonoBehaviour
         _carRigidbody.AddRelativeForce(Vector3.forward * (_horsePower * _forwardInput));
         // Rotates the Vehicle based on horizontal input.
         transform.Rotate(Vector3.up, turnSpeed * _horizontalInput * Time.deltaTime);
+        
+        // Calculates the speed in KMh and displays it in the gui.
+        _speed = _carRigidbody.velocity.magnitude * 3.6f; // Use 2.237 for mph.
+        speedometerText.text = $"Speed: {(int)_speed} kmh";
     }
 }
