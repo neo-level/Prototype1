4660eb9522cde2bc859034b6418cbbbd675b0f35
frederik desmet <info@neo-level.com>
2021-01-22 13:50:36 +0100

    Added an RPM display.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
     // Allows to interact with the variable in the unity editor while staying private.
-    [SerializeField] private float _speed;
-    [SerializeField] private float _horsePower = 0.0f;
-    
+    [SerializeField] private float speed;
+    [SerializeField] private float rpm;
+    [SerializeField] private float horsePower = 0.0f;
+
     [SerializeField] private GameObject centerOfMass;
     [SerializeField] private TextMeshProUGUI speedometerText;
-    
-    
+    [SerializeField] private TextMeshProUGUI rpmText;
+
+
     private float turnSpeed = 20;
     private float _horizontalInput;
     private float _forwardInput;
@@ -34,12 +37,16 @@ public class PlayerController : MonoBehaviour
         _forwardInput = Input.GetAxis("Vertical");
 
         // Move the vehicle based on Vertical input. using physics and the local system.
-        _carRigidbody.AddRelativeForce(Vector3.forward * (_horsePower * _forwardInput));
+        _carRigidbody.AddRelativeForce(Vector3.forward * (horsePower * _forwardInput));
         // Rotates the Vehicle based on horizontal input.
         transform.Rotate(Vector3.up, turnSpeed * _horizontalInput * Time.deltaTime);
-        
+
         // Calculates the speed in KMh and displays it in the gui.
-        _speed = _carRigidbody.velocity.magnitude * 3.6f; // Use 2.237 for mph.
-        speedometerText.text = $"Speed: {(int)_speed} kmh";
+        speed = _carRigidbody.velocity.magnitude * 3.6f; // Use 2.237 for mph.
+        speedometerText.text = $"Speed: {(int) speed} kmh";
+
+        // Calculate the RPM value
+        rpm = (speed % 30) * 40;
+        rpmText.SetText($"RPM: {(int) rpm}");
     }
 }
